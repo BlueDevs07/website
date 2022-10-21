f59270b6854b7a4026f4463f5b4d4c7cc440fd9c
Jugya Kamal Gogoi <97836029+Jugya07@users.noreply.github.com>
2022-10-22 00:50:39 +0530

    design components

--- components/GroupCard/GroupCard.jsx
+++ components/GroupCard/GroupCard.jsx
@@ -1,3 +1,16 @@
-const GroupCard = ({ data }) => {};
+import styles from "../GroupCard/GroupCard.module.css"
+import Link from "next/link";
 
+function GroupCard({data={},color=1,route="#"}){
+   return (
+    <Link href={route}>
+   <div className= {`${styles.container} ${color? "bg-[#A4D672]" : "bg-[#0007]"}`}>
+        <div className={styles.title}>
+            <img  className={styles.img} src="LargeCard/Rectangle.png"/>
+            {data?.title}
+        </div>
+    </div> 
+    </Link>
+   )
+}
 export default GroupCard;
---
+++ components/LargeCard/LargeCard.jsx
@@ -0,0 +1,22 @@
+import styles from "../LargeCard/LargeCard.module.css"
+
+function LargeCard({data={}}){
+    return(
+        <div className={styles.container}>
+                <img className={styles.backg} src="LargeCard/Rectangle.png"/>
+                <div className={styles.data}>
+                   <div className={styles.dataTitle}> 
+                        {data?.title}
+                    </div> 
+                    <div className={styles.dataContent}>
+                        {data?.content}
+                    </div> 
+                    </div>
+                <div className={styles.img}>
+                    <img src="favicon.ico" />
+                </div>
+        </div>
+    )
+}
+
+export default LargeCard
